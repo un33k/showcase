c259d57b3bd1ca8e62206c481d9d9247f3dcb952
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-06-05 18:54:28 -0400

    add mdi/font

---
+++ config/app.ts
@@ -0,0 +1,11 @@
+type appConfig = {
+  name: string
+  description: string
+  keywords: string
+}
+
+export const appConfig: appConfig = {
+  name: "Showcase",
+  description: "Showcase",
+  keywords: "Showcase",
+}
---
+++ libs/ui/navbar.tsx
@@ -0,0 +1,38 @@
+import { mdiDotsVertical, mdiMenu } from "@mdi/js"
+import Icon from "@mdi/react"
+
+import { appConfig } from "@/config/app"
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <Icon
+            path={mdiMenu}
+            title="Menu"
+            size={1}
+            horizontal
+            vertical
+            color="red"
+          />
+        </button>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">{appConfig.name}</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <Icon
+            path={mdiDotsVertical}
+            title="Menu"
+            size={1}
+            horizontal
+            vertical
+            color="red"
+          />
+        </button>
+      </div>
+    </div>
+  )
+}
