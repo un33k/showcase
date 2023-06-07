ba11db25374cd335be0247d3fe58e259f695975a
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-06-07 19:56:27 -0400

    add dark daisy

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="dark">
       <body className={`${interFont.className}`}>{children}</body>
     </html>
   )
