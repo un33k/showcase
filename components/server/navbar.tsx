d0a9a0ad5d34537332d23229e72d257eb3d58b1e
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-06-07 21:36:11 -0400

    server side rotate

--- components/server/navbar.tsx
+++ components/server/navbar.tsx
@@ -3,28 +3,25 @@
 import { mdiClose, mdiDelete, mdiDotsVertical, mdiMenu } from "@mdi/js"
 import Icon from "@mdi/react"
 import RotatingComponent from "@sc/libs/animation/rotate"
-import { useMorph } from "react-morph"
 
 export default function Navbar() {
-  const morph = useMorph()
   return (
     <div className="navbar bg-base-100">
       <div className="flex-none">
-        <RotatingComponent>
+        <RotatingComponent onRotate={() => console.log("Component rotated!")}>
           <button className="btn btn-square btn-ghost">
             <Icon path={mdiMenu} title="Menu" />
           </button>
         </RotatingComponent>
-
-        {/* <button {...morph} className="btn btn-square btn-ghost">
-          <Icon path={mdiClose} title="Menu" />
-        </button> */}
       </div>
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
       <div className="flex-none">
-        <RotatingComponent>
+        <RotatingComponent
+          rotationDegrees={90}
+          onRotate={() => console.log("Component rotated!")}
+        >
           <button className="btn btn-square btn-ghost">
             <Icon path={mdiDotsVertical} title="Options" />
           </button>
