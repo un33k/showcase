7b47677502e2081661f031de0004e682beb46bcf
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-06-10 19:15:25 -0400

    docs

--- typings/animation.ts
+++ typings/animation.ts
@@ -1,5 +1,12 @@
 import { ReactNode } from "react"
 
+/** Rotation component props
+ * @typedef {Object} RotateComponentProps
+ * @property {ReactNode} children
+ * @property {number} [rotationDegrees]
+ * @property {number} [rotationDuration]
+ * @property {() => void} [onClick]
+ */
 export interface RotateComponentProps {
   children: ReactNode
   rotationDegrees?: number
--- typings/app.ts
+++ typings/app.ts
@@ -1,3 +1,10 @@
+/**
+ * Application configuration.
+ * @typedef {Object} AppConfig
+ * @property {string} name
+ * @property {string} description
+ * @property {string} keywords
+ */
 export interface AppConfig {
   name: string
   description: string
