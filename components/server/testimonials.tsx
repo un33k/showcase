44d0fb96713a6c908427c5a6b35bc183b88fb6c6
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-06-13 14:48:03 -0400

    Localize Images, Lint

--- components/server/testimonials.tsx
+++ components/server/testimonials.tsx
@@ -1,11 +1,12 @@
+import Image from "next/image"
+
 const testimonials = [
   {
-    body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
+    body: "Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.",
     author: {
-      name: 'Leslie Alexander',
-      handle: 'lesliealexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      name: "Leslie Alexander",
+      handle: "lesliealexander",
+      imageUrl: "/images/image-girl.png",
     },
   },
   // More testimonials...
@@ -16,7 +17,9 @@ export default function Testimonials() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-xl text-center">
-          <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Testimonials</h2>
+          <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+            Testimonials
+          </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             We have worked with thousands of amazing people
           </p>
@@ -24,15 +27,26 @@ export default function Testimonials() {
         <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
           <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
             {testimonials.map((testimonial) => (
-              <div key={testimonial.author.handle} className="pt-8 sm:inline-block sm:w-full sm:px-4">
+              <div
+                key={testimonial.author.handle}
+                className="pt-8 sm:inline-block sm:w-full sm:px-4"
+              >
                 <figure className="rounded-2xl bg-gray-50 p-8 text-sm leading-6">
                   <blockquote className="text-gray-900">
                     <p>{`“${testimonial.body}”`}</p>
                   </blockquote>
                   <figcaption className="mt-6 flex items-center gap-x-4">
-                    <img className="h-10 w-10 rounded-full bg-gray-50" src={testimonial.author.imageUrl} alt="" />
+                    <Image
+                      className="h-10 w-10 rounded-full bg-gray-50"
+                      src={testimonial.author.imageUrl}
+                      alt=""
+                      width={40}
+                      height={40}
+                    />
                     <div>
-                      <div className="font-semibold text-gray-900">{testimonial.author.name}</div>
+                      <div className="font-semibold text-gray-900">
+                        {testimonial.author.name}
+                      </div>
                       <div className="text-gray-600">{`@${testimonial.author.handle}`}</div>
                     </div>
                   </figcaption>
