6d37e91a84ff011c73e215a2ecb62583e6dda5c3
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-03 18:56:38 -0400

    trigger build

--- packages/ui/src/__tests__/index.test.tsx
+++ packages/ui/src/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Navbar } from '../components/Navbar';
+import { Navbar } from '../components/navbar';
 
 describe('CounterButton', () => {
   it('renders without crashing', () => {
