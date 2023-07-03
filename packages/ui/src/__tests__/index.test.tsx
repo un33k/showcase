42b400f211f040d7680c2e7781a5d76fcf09dacc
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-03 13:59:37 -0400

    test

--- packages/ui/src/__tests__/index.test.tsx
+++ packages/ui/src/__tests__/index.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { CounterButton } from '../components/client/counterButton';
+import { Navbar } from '../components/Navbar';
 
 describe('CounterButton', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<CounterButton />, div);
+    ReactDOM.render(<Navbar />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
